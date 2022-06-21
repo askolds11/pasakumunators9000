5ad8aad7bc9143be9b8d2b971a7abb5f4b711db1
askolds <askolds11@gmail.com>
2022-06-21 15:17:50 +0300

    Added Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return 'Test page!';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
