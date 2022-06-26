0b1679cee61b5fef51fdc887ccb8bea36e9a226a
JJ <60788469+JJeris@users.noreply.github.com>
2022-06-26 19:01:00 +0200

    Changes to mainpage:(Mums nestrādā css styles; varbūt kaut kas saistītā ar link)

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles-mainpage.css">
+    <!-- NESTRĀDĀ STYLEES LINK UN ES NESAPROTU KĀDĒĻ - Justs -->
+    <link rel="stylesheet" href="../css/styles-mainpage.php">
+    <!-- GOOGLE FONT LINKS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">   
     <title>Pasākumunators9000</title>
 </head>
 <body>
@@ -14,13 +18,15 @@
     </header>
   
     <nav id="navbar">
+        
         <ul class="nav-links">
             <li><a href="">JAUNUMI</a></li>
             <li><a href="">FILTRĒT</a></li></li>
-            <li><a href="">JAUNUMI</a></li>
-            <li><a href="">FILTRĒT</a></li></li>
+            <li><a href="">REGISTER</a></li>
+            <li><a href="">LOGIN</a></li></li>
             
         </ul>
+        
         <!-- <button><a href="">REGISTER</a></button>
         <button><a href="">LOGIN</a></button> -->
     </nav>
