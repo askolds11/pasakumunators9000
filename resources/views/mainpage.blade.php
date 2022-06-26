c391d5b965b1e0395317f998c9a6c3b60bbd7d99
JJ <60788469+JJeris@users.noreply.github.com>
2022-06-26 17:43:05 +0200

    Updates mainpage.blade.php un styles-mainpage.css

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -1,29 +1,40 @@
 <!DOCTYPE html>
-<html lang="lv">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles-mainpage.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>Pasākumunators9000</title>
 </head>
 <body>
     <header id="header-project-name">
-        <p>AlmostPasākumunators9000</p>
+        <p>Pasākumunators9000</p>
     </header>
-    <nav>
-        <ul>
-            <li>JAUNUMI</li>
-            <li>FILTRĒT</li>
-            <li>LOGIN</li>
-            <li>REGISTER</li>
+  
+    <nav id="navbar">
+        <ul class="nav-links">
+            <li><a href="">JAUNUMI</a></li>
+            <li><a href="">FILTRĒT</a></li></li>
+            <li><a href="">JAUNUMI</a></li>
+            <li><a href="">FILTRĒT</a></li></li>
+            
         </ul>
-        
+        <!-- <button><a href="">REGISTER</a></button>
+        <button><a href="">LOGIN</a></button> -->
     </nav>
+  
+    
+    <section id="tituli">
 
+    </section>
+    <section id="komentari">
 
+    </section>
+
+    <footer id="footer">
+        <p>Veidoja Askolds Bērziņš, Johans Justs Eris, Aleksejs Romaņuks</p>
+    </footer>
 </body>
 </html>
