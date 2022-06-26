59de74e053dd5ddb40a2c63284c6d4d5beed5bd0
JJ <60788469+JJeris@users.noreply.github.com>
2022-06-26 15:29:01 +0200

    Pievienoju mainpage.blade.php - tikko iesākts.

---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles-mainpage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Pasākumunators9000</title>
+</head>
+<body>
+    <header id="header-project-name">
+        <p>AlmostPasākumunators9000</p>
+    </header>
+    <nav>
+        <ul>
+            <li>JAUNUMI</li>
+            <li>FILTRĒT</li>
+            <li>LOGIN</li>
+            <li>REGISTER</li>
+        </ul>
+        
+    </nav>
+
+
+</body>
+</html>
