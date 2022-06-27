352f0b1b29bfe61440fd732dd31c271665ffda02
askolds <askolds11@gmail.com>
2022-06-27 17:48:57 +0300

    Added resource controllers for comments and events Added code for the methods, unknown if works

---
+++ app/Http/Controllers/KomentarsController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KomentarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($id)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /********
+            $table->foreignId('lietotajs_id')->constrained('lietotajs');
+            $table->foreignId('pasakums_id')->constrained('pasakums');
+            $table->string('teksts',200);
+            $table->date('datums')->format('d/m/Y');
+        *********/
+
+        //Need auth check
+        //Need language
+
+        dd($request); //for testing
+
+        $lietotajs_id = $request->lietotajs_id; //may be done differently - to be seen
+        $pasakums_id = $request->pasakums_id;
+        $teksts = $request->teksts;
+        $datums = $request->datums;
+        
+
+        $rules = array(
+            'lietotajs_id' => 'required|exists:users|integer', //users table & lietotajs table, dsk?
+            'pasakums_id' => 'required|exists:pasakums|integer', //dsk
+            'teksts' => 'required|string|min:1|max:200',
+            'datums' => 'required|date|after:yesterday',
+        );
+        $this->validate($request, $rules);
+
+        $komentars = new Komentars();
+        $komentars->lietotajs_id = $request->lietotajs_id;
+        $komentars->pasakums_id = $request->pasakums_id;
+        $komentars->teksts = $request->teksts;
+        $komentars->datums = $request->datums; //May be an error
+        $komentars->save();
+
+        return redirect('pasakums/' . $pasakums_id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $komentars = Komentars::findOrFail($id);
+        return view('update_komentars', compact('komentars'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $komentars = Komentars::findOrFail($id);
+
+        $lietotajs_id = $request->lietotajs_id; //may be done differently - to be seen, does it need to change in update?
+        $pasakums_id = $request->pasakums_id;
+        $teksts = $request->teksts;
+        $date = $request->date;
+        
+
+        $rules = array(
+            'lietotajs_id' => 'required|exists:users|numeric', //users table & lietotajs table, dsk?
+            'pasakums_id' => 'required|exists:pasakums|numeric', //dsk
+            'teksts' => 'required|string|min:1|max:200',
+            'date' => 'required|date|after:yesterday',
+        );
+        $this->validate($request, $rules);
+
+        $komentars->lietotajs_id = $request->lietotajs_id;
+        $komentars->pasakums_id = $request->pasakums_id;
+        $komentars->teksts = $request->teksts;
+        $komentars->date = $request->date; //May be an error
+        $komentars->save();
+
+        return redirect('pasakums/' . $pasakums_id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pasakums_id = Komentars::findOrFail($id)->pasakums_id;
+        Komentars::findOrFail($id)->delete();
+        return redirect('pasakums/' . $pasakums_id);
+    }
+}
---
+++ app/Http/Controllers/PasakumsController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasakumsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('new_pasakums');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /**********
+            $table->string('nosaukums',50);
+            $table->string('apraksts',100);
+            $table->date('datums')->format('d/m/Y');
+            $table->time('norises_ilgums');
+            $table->string('norises_vieta',100);
+            $table->decimal('cena',5,2);
+            $table->foreignId('veidotajs')->constrained('lietotajs');
+            $table->foreignId('kategorija_id')->constrained('kategorija');
+        **********/
+
+        //Need auth check
+        //Need language
+
+        dd($request); //for testing
+
+        $nosaukums = $request->nosaukums;
+        $apraksts = $request->apraksts;
+        $datums = $request->datums;
+        $norises_ilgums = $request->norises_ilgums;
+        $norises_vieta = $request->norises_vieta;
+        $cena = $request->cena;
+        $veidotajs = $request->veidotajs; //may be different - checking which user
+        $kategorijas = $request->kategorijas;
+        
+
+        $rules = array(
+            'nosaukums' => 'required|string|min:1|max:50',
+            'apraksts' => 'required|string|min:1|max:100',
+            'datums' => 'required|date|after:yesterday',
+            'norises_ilgums' => 'required|date_format:H:i',
+            'norises_vieta' => 'required|string|min:1|max:100',
+            'cena' => 'required|min:0|max:999.99',
+            'veidotajs' => 'required|exists:users|integer', //users table & lietotajs table, dsk?
+            'kategorijas.*' => 'required|exists:kategorija|integer', //dsk
+        );
+        $this->validate($request, $rules);
+
+        $pasakums = new Pasakums();
+        $pasakums->nosaukums = $request->nosaukums;
+        $pasakums->apraksts = $request->apraksts;
+        $pasakums->datums = $request->datums;
+        $pasakums->norises_ilgums = $request->norises_ilgums;
+        $pasakums->norises_vieta = $request->norises_vieta;
+        $pasakums->cena = $request->cena;
+        $pasakums->veidotajs = $request->veidotajs; //may be different - checking which user
+        $pasakums->kategorijas = $request->kategorijas;
+        $pasakums->save();
+
+        return redirect('pasakums/' . $pasakums->id); //not sure if id exists
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $pasakums = Pasakums::findOrFail($id);
+        return view('update_pasakums', compact('pasakums'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $pasakums = Pasakums::findOrFail($id);
+
+        $nosaukums = $request->nosaukums;
+        $apraksts = $request->apraksts;
+        $datums = $request->datums;
+        $norises_ilgums = $request->norises_ilgums;
+        $norises_vieta = $request->norises_vieta;
+        $cena = $request->cena;
+        $veidotajs = $request->veidotajs; //may be different - checking which user, does it need to change in update?
+        $kategorijas = $request->kategorijas;
+        
+
+        $rules = array(
+            'nosaukums' => 'required|string|min:1|max:50',
+            'apraksts' => 'required|string|min:1|max:100',
+            'datums' => 'required|date|after:yesterday',
+            'norises_ilgums' => 'required|date_format:H:i',
+            'norises_vieta' => 'required|string|min:1|max:100',
+            'cena' => 'required|min:0|max:999.99',
+            'veidotajs' => 'required|exists:users|integer', //users table & lietotajs table, dsk?
+            'kategorijas.*' => 'required|exists:kategorija|integer', //dsk
+        );
+        $this->validate($request, $rules);
+
+        $pasakums->nosaukums = $request->nosaukums;
+        $pasakums->apraksts = $request->apraksts;
+        $pasakums->datums = $request->datums;
+        $pasakums->norises_ilgums = $request->norises_ilgums;
+        $pasakums->norises_vieta = $request->norises_vieta;
+        $pasakums->cena = $request->cena;
+        $pasakums->veidotajs = $request->veidotajs; //may be different - checking which user
+        $pasakums->kategorijas = $request->kategorijas;
+        $pasakums->save();
+
+        return redirect('pasakums/' . $pasakums->id); //not sure if id exists
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Will need to delete registration entries
+        Komentars::where('pasakums_id',$id)->delete();
+        Pasakums::findOrFail($id)->delete();
+        return redirect('mainpage');
+    }
+}
