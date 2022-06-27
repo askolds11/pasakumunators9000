0d1ba73cf30daa10bd7501e05dc37d7bb8b46db8
JJ <60788469+JJeris@users.noreply.github.com>
2022-06-27 03:58:04 +0200

    Filter un tā css (TIKS OBLIGĀTI MAINĪTS)

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="lv">
+<!DOCTYPE html>
+<html lang="lv">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- STYLESHEETS -->
-     <link rel="stylesheet" href="{{url('css/styles-filter.css')}}">
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="{{url('css/styles-filter.css')}}">
     <!-- GOOGLE FONT LINKS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,6 +16,37 @@
     <title>Filters</title>
 </head>
 <body>
-    <p>red</p>
+    <header id="header-project-name">
+        <p>Pasākumunators9000</p>
+    </header>
+    <div id="filter-pasakumu">
+        <h2>FILTRĒT PASĀKUMU</h2>
+        <br>
+        <form action="">
+        <label for="">1. kritērija:</label><br>
+        <input type="text" id="" name="" value=""><br>
+        <label for="lname">2. kritērija</label><br>
+        <input type="text" id="" name="" value=""><br>
+        <label for="lname">3. kritērija</label><br>
+        <input type="text" id="" name="" value=""><br>
+        <label for="lname">4. kritērija</label><br>
+        <input type="radio" id="" name="" value="">
+        <label for="">1. opcija</label><br>
+        <input type="radio" id="" name="" value="">
+        <label for="">2. opcija</label><br>
+        <input type="radio" id="" name="" value="">
+        <label for="">3. opcija</label><br><br>
+        <input type="submit" value="Meklēt">
+        </form>
+    </div>
+    <div id="results">
+        <h3>REZULTĀTI</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique modi, deserunt ab architecto necessitatibus enim magni, reprehenderit numquam saepe dolore natus inventore aut fuga voluptatibus maiores aperiam, neque eligendi nam qui quo doloribus labore nisi soluta? Impedit aut ratione mollitia exercitationem error voluptatum. Minima debitis hic animi dicta enim eos vero. Possimus repellendus officia quaerat commodi eligendi adipisci saepe aliquid dicta dolores, fugiat debitis soluta veritatis reiciendis at tempore enim quidem corrupti sed. Repellendus pariatur obcaecati, sunt accusamus sequi consectetur numquam quidem iste! Assumenda ad fugit aliquam accusamus quidem et non cumque quisquam reprehenderit inventore maiores dolorem, animi placeat, cum corrupti deserunt blanditiis quis minus dolor nihil. In dolore voluptas perspiciatis delectus, exercitationem consequuntur dolorem dolorum aspernatur alias fugiat iste aut earum doloribus ratione voluptatem! Adipisci enim, deleniti eos, rerum magni eaque aspernatur aliquid temporibus doloribus molestiae odio, animi qui assumenda. Ut laboriosam voluptate tenetur. Iure, enim reiciendis. Pariatur eaque nesciunt consectetur labore mollitia ipsa eos, voluptas quis officia, asperiores, ullam quidem. Tempore perspiciatis odio recusandae ipsa repellendus neque assumenda exercitationem quaerat iste blanditiis similique nemo ullam aut cupiditate dolorem eveniet, animi natus voluptates quidem dolore quibusdam corporis! Soluta maxime hic labore dolor vel iste beatae iusto repellendus officia.</p>
+
+    </div>
+    <footer id="footer">
+        <p>Veidoja: Askolds Bērziņš, Johans Justs Eris, Aleksejs Romaņuks</p>
+        <p>&copy Copyright AskoldsJustsAleksejs inc.</p>
+    </footer>
 </body>
 </html>
