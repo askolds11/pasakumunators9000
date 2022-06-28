1bfa57036296c60bd0bc908e43c8909e804ef7e0
askolds <askolds11@gmail.com>
2022-06-28 22:55:02 +0300

    AdminPanelController now passes roles as an array Username must be unique validation

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -11,11 +11,27 @@ class AdminPanelController extends Controller
 {
     public function index()
     {
-        $users = User::join('lietotajsloma', 'lietotajsloma.users_id', '=', 'users.id')
+        $usersroles = User::join('lietotajsloma', 'lietotajsloma.users_id', '=', 'users.id')
                             ->join('loma', 'lietotajsloma.loma_id', '=', 'loma.id')
-                            ->select('users.id as user.id', 'users.name as user.name', 'loma.id as loma.id', 'loma.nosaukums as loma.nosaukums')
+                            ->select('users.id as userid', 'users.name as username', 'loma.id as lomaid', 'loma.nosaukums as lomanosaukums')
                             ->orderBy('users.name', 'asc')
                             ->get();
+
+        $users = [];
+        foreach(User::orderBy('users.name', 'asc')->get() as $user)
+        {
+            $users[$user->name]['user.id'] = $user->id;
+            $users[$user->name]['user.name'] = $user->name;
+            $users[$user->name]['roles'] = [];
+            foreach($usersroles as $userrole)
+            {
+                if($userrole->username == $user->name)
+                {
+                    $role = array('lomaid'=>$userrole['lomaid'],'lomanosaukums'=>$userrole['lomanosaukums']);
+                    $users[$user->name]['roles'][$userrole->lomanosaukums] = $role;
+                }
+            }
+        }
 
         $comments = Komentars::orderBy('updated_at', 'desc')
                             ->orderBy('id', 'desc')
