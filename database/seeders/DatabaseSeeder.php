d4c8a8e3b44c38cff17adb382908deeb8a3637c0
qromanuk <aleksejs.romanuks@gmail.com>
2022-06-30 02:13:12 +0300

    added pictures/seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -59,24 +59,59 @@ class DatabaseSeeder extends Seeder
             'email'=> 'maijaliepa@gmail.com',
             'password'=> bcrypt('maijaliepa123')
         ]);
+        $users = User::create([
+            'name'=> 'dianataurina',
+            'email'=> 'dianataurina@gmail.com',
+            'password'=> bcrypt('dianataurina123')
+        ]);
+        $users = User::create([
+            'name'=> 'katarinabush',
+            'email'=> 'katarinabush@gmail.com',
+            'password'=> bcrypt('katarinabush123')
+        ]);
+        $users = User::create([
+            'name'=> 'lindamaurina',
+            'email'=> 'lindamaurina@gmail.com',
+            'password'=> bcrypt('lindamaurina123')
+        ]);
+
 
         //Kategorija
 
         $kategorija = Kategorija::create([
-            'nosaukums'=>'futbols'
+            'nosaukums'=>'Bokss'
         ]);
         $kategorija = Kategorija::create([
-            'nosaukums'=>'basketbols'
+            'nosaukums'=>'Beisbols'
         ]);
         $kategorija = Kategorija::create([
-            'nosaukums'=>'volejbols'
+            'nosaukums'=>'Boulings'
         ]);
         $kategorija = Kategorija::create([
-            'nosaukums'=>'peldesana'
+            'nosaukums'=>'Florbols'
         ]);
         $kategorija = Kategorija::create([
-            'nosaukums'=>'florbols'
+            'nosaukums'=>'Futbols'
         ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Galda teniss'
+        ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Golfs'
+        ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Kartings'
+        ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Peintbols'
+        ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Skeitbords'
+        ]);
+        $kategorija = Kategorija::create([
+            'nosaukums'=>'Teniss'
+        ]);
+        
         
         //Loma
         $loma = Loma::create([
@@ -99,7 +134,7 @@ class DatabaseSeeder extends Seeder
 
         $pasakums = Pasakums::create([
             'nosaukums'=>'Rigas futbola turnirs',
-            'apraksts'=>'Ikgadejais futbola ternirs ks notiek Riga start LU un RTU universitatem',
+            'apraksts'=>'Ikgadejais futbola turnirs kas notiek Riga: starp LU un RTU universitatem',
             'datums'=> Carbon::parse('2022-07-06 14:40:00'),
             'norises_ilgums'=>'30',
             'norises_vieta'=>'Kipsalas sporta centra',
@@ -108,8 +143,8 @@ class DatabaseSeeder extends Seeder
             'attels_id' => '1'
             ]);
         $pasakums = Pasakums::create([
-            'nosaukums'=>'Olaines basketbola cempionats',
-            'apraksts'=>'2022 Olaines basketbola cempionts start 9-12 klases skoleniem',
+            'nosaukums'=>'Olaines Florbola cempionats',
+            'apraksts'=>'2022 Olaines Florbola cempionats starp 9-12 klases skoleniem',
             'datums'=> Carbon::parse('2022-09-12 12:00:00'),
             'norises_ilgums'=>'120',
             'norises_vieta'=>'Olaines sporta centra',
@@ -118,15 +153,45 @@ class DatabaseSeeder extends Seeder
             'attels_id' => '1'
             ]);
         $pasakums = Pasakums::create([
-            'nosaukums'=>'Jelgavas peldesanas sacensibas',
-            'apraksts'=>'Atklatas Jelgavas peldesanas sacensibas',
+            'nosaukums'=>'Jelgavas beisbola sacensibas',
+            'apraksts'=>'Atklatas Jelgavas beisbola sacensibas',
             'datums'=>Carbon::parse('2022-11-21 10:30:00'),
             'norises_ilgums'=>'60',
-            'norises_vieta'=>'Jelgavas baseins',
+            'norises_vieta'=>'Jelgavas centrala parka',
             'cena'=>'3.00',
             'veidotajs_id'=>'2',
             'attels_id' => '1'
             ]);
+        $pasakums = Pasakums::create([
+            'nosaukums'=>'Rigas galda tenisa 2022 atlases spele',
+            'apraksts'=>'Rigas 5-17g. jauniešu galda tenisa atlases spele nosledzot 2022 gada seriju',
+            'datums'=>Carbon::parse('2022-11-21 15:00:00'),
+            'norises_ilgums'=>'40',
+            'norises_vieta'=>'VEF kulturas centrs',
+            'cena'=>'3.00',
+            'veidotajs_id'=>'3',
+            'attels_id' => '1'
+            ]);
+        $pasakums = Pasakums::create([
+            'nosaukums'=>'Daugavpls golfa sacensibas',
+            'apraksts'=>'Daugavpils sporta pili pilsetas sacensibas golfa ',
+            'datums'=>Carbon::parse('2022-12-11 12:30:00'),
+            'norises_ilgums'=>'30',
+            'norises_vieta'=>'Daugavpls sporta pils',
+            'cena'=>'3.00',
+            'veidotajs_id'=>'4',
+            'attels_id' => '1'
+            ]); 
+        $pasakums = Pasakums::create([
+            'nosaukums'=>'Boulings visiem',
+            'apraksts'=>'Open-Air boulinga turnirs - Eiropas kvalifikacija',
+            'datums'=>Carbon::parse('2023-01-17 11:00:00'),
+            'norises_ilgums'=>'90',
+            'norises_vieta'=>'Riga, Mezaparka',
+            'cena'=>'3.00',
+            'veidotajs_id'=>'5',
+            'attels_id' => '1'
+            ]);   
 
             //Komentars
         $komentars = Komentars::create([
@@ -147,6 +212,27 @@ class DatabaseSeeder extends Seeder
             'teksts'=>'Slikta edinasana',
             //'datums'=>Carbon::parse('2022-12-12')
         ]);
+        $komentars = Komentars::create([
+            'users_id'=>'4',
+            'pasakums_id'=>'4',
+            'teksts'=>'Profesionala attieksme pret dalibniekiem un viesiem',
+        ]);
+        $komentars = Komentars::create([
+            'users_id'=>'5',
+            'pasakums_id'=>'5',
+            'teksts'=>'Katru gadu piedalos - vienmer labakie rezultati',
+        ]);
+        $komentars = Komentars::create([
+            'users_id'=>'2',
+            'pasakums_id'=>'2',
+            'teksts'=>'Labi',
+        ]);
+        $komentars = Komentars::create([
+            'users_id'=>'3',
+            'pasakums_id'=>'3',
+            'teksts'=>'Pasākumu organizēšanā ir profesionāla komanda. Esam priecīgi, gandarīti un pateicīgi par sadarbību.',
+        ]);
+
 
         //LietotajsPasakums
         $lietotajspasakums = LietotajsPasakums::create([
@@ -161,6 +247,22 @@ class DatabaseSeeder extends Seeder
             'users_id'=>'3',
             'pasakums_id'=>'3'
         ]);
+        $lietotajspasakums = LietotajsPasakums::create([
+            'users_id'=>'4',
+            'pasakums_id'=>'4'
+        ]);
+        $lietotajspasakums = LietotajsPasakums::create([
+            'users_id'=>'5',
+            'pasakums_id'=>'5'
+        ]);
+        $lietotajspasakums = LietotajsPasakums::create([
+            'users_id'=>'6',
+            'pasakums_id'=>'6'
+        ]);
+        $lietotajspasakums = LietotajsPasakums::create([
+            'users_id'=>'7',
+            'pasakums_id'=>'6'
+        ]);
 
         //LietotajsLoma
         $lietotajsloma = LietotajsLoma::create([
@@ -168,13 +270,27 @@ class DatabaseSeeder extends Seeder
             'loma_id'=>'1'
         ]);
         $lietotajsloma = LietotajsLoma::create([
-            'users_id'=>'1',
+            'users_id'=>'3',
             'loma_id'=>'2'
         ]);
         $lietotajsloma = LietotajsLoma::create([
-            'users_id'=>'3',
+            'users_id'=>'4',
             'loma_id'=>'1'
         ]);
+        $lietotajsloma = LietotajsLoma::create([
+            'users_id'=>'5',
+            'loma_id'=>'2'
+        ]);
+        $lietotajsloma = LietotajsLoma::create([
+            'users_id'=>'6',
+            'loma_id'=>'1'
+        ]);
+        $lietotajsloma = LietotajsLoma::create([
+            'users_id'=>'7',
+            'loma_id'=>'1'
+        ]);
+
+
 
         //admin
         $lietotajsloma = LietotajsLoma::create([
@@ -193,53 +309,99 @@ class DatabaseSeeder extends Seeder
         //Novertejums
         $novertejums = Novertejums::create([
             'users_id'=>'1',
-            'pasakums_id'=>'3',
+            'pasakums_id'=>'1',
             'novertejums'=>'8'
         ]);
         $novertejums = Novertejums::create([
             'users_id'=>'2',
-            'pasakums_id'=>'1',
+            'pasakums_id'=>'2',
             'novertejums'=>'7'
         ]);
         $novertejums = Novertejums::create([
             'users_id'=>'3',
-            'pasakums_id'=>'1',
+            'pasakums_id'=>'3',
             'novertejums'=>'2'
         ]);
-        //Attels
+        $novertejums = Novertejums::create([
+            'users_id'=>'4',
+            'pasakums_id'=>'4',
+            'novertejums'=>'3'
+        ]);
+        $novertejums = Novertejums::create([
+            'users_id'=>'5',
+            'pasakums_id'=>'5',
+            'novertejums'=>'6'
+        ]);
+        $novertejums = Novertejums::create([
+            'users_id'=>'6',
+            'pasakums_id'=>'6',
+            'novertejums'=>'9'
+        ]);
+        //Attels*****************
         $attels = Attels::create([
-            'apraksts'=>'Bilde no Lienes',
+            'apraksts'=>'futbola turnirs',
             'datums'=>Carbon::parse('2022-10-10'),
             'pasakums_id'=>'1',
-            'picture'=> ('public/images/image_1.jpg')
+            'picture'=> ('public/images/futbols_1.jpg')
         ]);
         $attels = Attels::create([
-            'apraksts'=>'Interesanti',
+            'apraksts'=>'Florbola cempionats',
             'datums'=>Carbon::parse('2022-10-12'),
             'pasakums_id'=>'2',
-            'picture'=> ('public/images/image_2.jpg')
+            'picture'=> ('public/images/florbols_1.jpg')
         ]);
 
         $attels = Attels::create([
-            'apraksts'=>'Es un mani draugi',
+            'apraksts'=>'beisbola sacensibas',
             'datums'=>Carbon::parse('2022-12-22'),
             'pasakums_id'=>'3',
-            'picture'=> ('public/images/image_3.jpg')
+            'picture'=> ('public/images/beisbols_1.jpg')
+        ]);
+        $attels = Attels::create([
+            'apraksts'=>'galda tenisa 2022 atlases spele',
+            'datums'=>Carbon::parse('2022-11-27'),
+            'pasakums_id'=>'4',
+            'picture'=> ('public/images/galda_teniss_1.jpg')
+        ]);
+        $attels = Attels::create([
+            'apraksts'=>'golfa sacensibas',
+            'datums'=>Carbon::parse('2023-08-15'),
+            'pasakums_id'=>'5',
+            'picture'=> ('public/images/golfs.jpg')
+        ]);
+        $attels = Attels::create([
+            'apraksts'=>'boulinga turnirs',
+            'datums'=>Carbon::parse('2023-01-12'),
+            'pasakums_id'=>'6',
+            'picture'=> ('public/images/bowling_1.JPG')
         ]);
 
         //PasakumsKategorija
         $pasakumskategorija = PasakumsKategorija::create([
             'pasakums_id'=>'1',
-            'kategorija_id'=>'2'
+            'kategorija_id'=>'5'
         ]);
         $pasakumskategorija = PasakumsKategorija::create([
             'pasakums_id'=>'2',
-            'kategorija_id'=>'3'
+            'kategorija_id'=>'4'
         ]);
         $pasakumskategorija = PasakumsKategorija::create([
             'pasakums_id'=>'3',
-            'kategorija_id'=>'1'
+            'kategorija_id'=>'2'
         ]);
+        $pasakumskategorija = PasakumsKategorija::create([
+            'pasakums_id'=>'4',
+            'kategorija_id'=>'6'
+        ]);
+        $pasakumskategorija = PasakumsKategorija::create([
+            'pasakums_id'=>'5',
+            'kategorija_id'=>'7'
+        ]);
+        $pasakumskategorija = PasakumsKategorija::create([
+            'pasakums_id'=>'6',
+            'kategorija_id'=>'3'
+        ]);
+
 
 
         
