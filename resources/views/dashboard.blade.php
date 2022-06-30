17b8e940d56a0d87dd4fae6680e0a3f14a948f0d
JJ <60788469+JJeris@users.noreply.github.com>
2022-07-01 01:42:03 +0200

    Views update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,19 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layout_dashboard')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+@section('title', 'Reset')
+
+@section('content')
+<div id="dashboard-content">
+        <div id="about-div">
+            <div id="about-content-group">
+                <h2 class="about-content">Jūs esiet ierakstījies savā kontā - {{ Auth::user()->name }}!</h2>
+                <h2 class="about-content">Paldies, ka lietojat mūsu mājaslapu! :D</h2>
+            
+            
             </div>
         </div>
-    </div>
-</x-app-layout>
+    
+
+</div>
+@endsection
+
