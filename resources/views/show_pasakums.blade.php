1bcff8a00daf87b6d357375b5d145021ea603c56
JJ <60788469+JJeris@users.noreply.github.com>
2022-06-30 16:05:51 +0200

    Mainpaage almost finished. show_pasakums not done, but updated

--- resources/views/show_pasakums.blade.php
+++ resources/views/show_pasakums.blade.php
@@ -10,10 +10,19 @@
         <h2 id="title-show-pasakums">Title</h2>
         <ul>
             <li id="show-user-name-pasakums-publicetajs">
-                <p id="show-user-name-pasakums"><a href="" class="link-to-publicetajs">User2</a></p>
-                <p id="show-datums-publicets-pasakums">09.10.2020.</p>
-                <p id="show-laiks-publicets-pasakums">12:00:00</>
+                <div id=show-pasākuma-details>
+                    <p id="show-user-name-pasakums"><a href="" class="link-to-publicetajs">User2</a></p>
+                    <p id="show-datums-publicets-pasakums">09.10.2020.</p>
+                    <p id="show-laiks-publicets-pasakums">12:00:00</>
+                </div>
+                <div id=show-edit-delete-link>
+                    <a href="" id="show-edit-link">Edit</a>
+                    <a href="" id="show-delete-link">Delete</a>
+                </div>
+                
             </li>
+        </ul>
+        <ul>
             <li id="apraksts-show-pasakums" class="show-pasakuma-info">
                 <h3>
                     Apraksts
