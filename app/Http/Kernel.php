edab69148a56c7932c38d5c1692176d1cbda2e80
askolds <askolds11@gmail.com>
2022-07-01 04:32:34 +0300

    Log correct middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\LogAll::class,
     ];
 
     /**
@@ -38,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\RejectBanned::class,
+            \App\Http\Middleware\LogAll::class,
         ],
 
         'api' => [
